809766742b2ff4cbff3c007e45ee3ba0711b7e5f
Guru200616 <168887288+Guru200616@users.noreply.github.com>
2026-01-04 17:44:58 +0530

    Create portfolio.js with user details and skills
    Added greeting, social media links, and skills section.

---
+++ src/portfolio.js
@@ -0,0 +1,29 @@
+export const greeting = {
+  username: "Saad Pasta",
+  title: "Hi all, I'm Saad 👋",
+  subTitle: "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native.",
+  resumeLink: "#",
+};
+
+export const socialMediaLinks = {
+  github: "https://github.com",
+  linkedin: "https://linkedin.com",
+  gmail: "mailto:saad@example.com",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    "⚡ Develop highly interactive Front end / User Interfaces",
+    "⚡ Progressive Web Applications (PWA) and SPA Stacks",
+    "⚡ Integration of Firebase, AWS, and Cloud Services"
+  ],
+  softwareSkills: [
+    { name: "HTML5", fontClass: "fab fa-html5", color: "#E34F26" },
+    { name: "CSS3", fontClass: "fab fa-css3-alt", color: "#1572B6" },
+    { name: "JavaScript", fontClass: "fab fa-js", color: "#F7DF1E" },
+    { name: "React", fontClass: "fab fa-react", color: "#61DAFB" },
+    { name: "NodeJS", fontClass: "fab fa-node", color: "#339933" },
+  ]
+};
